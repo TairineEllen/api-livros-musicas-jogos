2a45266ef127cf08af926ce09f9b7642bf555f88
TairineEllen <tairine.ellen@gmail.com>
2020-10-28 18:09:05 -0300

    created function to access a specific series by its id (/series/id)

--- src/controllers/controllerSeries.js
+++ src/controllers/controllerSeries.js
@@ -2,8 +2,22 @@ const series = require('../models/series.json');
 
 const getAllSeries = (req, res) => {
   res.status(200).send(series);
-};  
+};
+
+const getSerieById = (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const serieSelected = series.find(serie => serie.id == id);
+
+    serieSelected ? res.status(200).send(serieSelected) : res.status(404).send('Série não encontrado');
+            
+  } catch (error) {
+    res.status(424).send('Erro interno do servidor');    
+  };
+};
 
 module.exports = {
   getAllSeries,
+  getSerieById
 };
--- src/routes/routeSeries.js
+++ src/routes/routeSeries.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/controllerSeries');
 
 router.get('/', controller.getAllSeries);
+router.get('/:id', controller.getSerieById);
 
 module.exports = router;
